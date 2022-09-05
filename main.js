67df27e41ae0fb57ac41b6438f812048be42e51f
taldr27 <diegogarciaq98@gmail.com>
2022-09-05 15:37:24 -0500

    Fix hamburger button with js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("button").onclick = function() {myFunction()};
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
-  document.getElementsByClassName("menu").style.display("flex");
+  var x = document.getElementById("hide-menu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
 }
