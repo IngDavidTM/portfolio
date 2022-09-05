604a7de8afc18ef94d7953bbf7424aac2a12be75
IngDavidTM <david-1011@hotmail.com>
2022-09-05 18:51:19 -0500

    fix linter errors on js and css

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-function myFunction() { 
-  var x = document.getElementById("hide-menu");
-  var y = document.querySelector('.toolbar');
-  if (x.style.display === "flex") { 
-    x.style.display = "none"; 
-    y.style.display = "flex";
-  } else { 
-    x.style.display = "flex"; 
-    y.style.display = "none";
-  } 
-}
- 
+function myFunction() { let x = document.getElementById('hide-menu'); let y = document.querySelector('.toolbar'); if (x.style.display === 'flex') { x.style.display = 'none'; y.style.display = 'flex'; } else { x.style.display = 'flex'; y.style.display = 'none'; } }
