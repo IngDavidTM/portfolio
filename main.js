05afd12d81dc4516e54d3a9e1236bb0382793f04
IngDavidTM <david-1011@hotmail.com>
2022-09-06 10:13:49 -0500

    fix code errors

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-function myFunction() { const x = document.getElementById('hide-menu'); const y = document.querySelector('.toolbar'); if (x.style.display === 'flex') { x.style.display = 'none'; y.style.display = 'flex'; } else { x.style.display = 'flex'; y.style.display = 'none'; } }
-document.getElementById('but').addEventListener('click', myFunction);
+function myFunction() {
+  const x = document.getElementById('hide-menu');
+  const y = document.querySelector('.toolbar');
+  if (x.style.display === 'flex') {
+    x.style.display = 'none'; y.style.display = 'flex';
+  } else {
+    x.style.display = 'flex';
+    y.style.display = 'none';
+  }
+}
+const boxes = document.querySelectorAll('.but');
+boxes.forEach((box) => { box.addEventListener('click', myFunction); });
