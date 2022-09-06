eaffd539908acedb09a42fa4bea40a155b5e7a66
taldr27 <diegogarciaq98@gmail.com>
2022-09-06 17:09:12 -0500

    Add javascript to the projects popup

--- main.js
+++ main.js
@@ -1,12 +1,125 @@
-function myFunction() {
-  const x = document.getElementById('hide-menu');
-  const y = document.querySelector('.toolbar');
-  if (x.style.display === 'flex') {
-    x.style.display = 'none'; y.style.display = 'flex';
-  } else {
-    x.style.display = 'flex';
-    y.style.display = 'none';
+window.addEventListener('DOMContentLoaded', () => {
+  function myFunction() {
+    const x = document.getElementById('hide-menu');
+    const y = document.querySelector('.toolbar');
+    if (x.style.display === 'flex') {
+      x.style.display = 'none'; y.style.display = 'flex';
+    } else {
+      x.style.display = 'flex';
+      y.style.display = 'none';
+    }
   }
-}
-const boxes = document.querySelectorAll('.but');
-boxes.forEach((box) => { box.addEventListener('click', myFunction); });
+  const boxes = document.querySelectorAll('.but');
+  boxes.forEach((box) => { box.addEventListener('click', myFunction); });
+  
+  const projects = [
+    {
+      title: 'Project 1',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on railsds',
+      badge2: 'CSSsd',
+      badge3: 'JavaScriptsd',
+      badge4: 'HTMLsd',
+      description: 'asdasLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      buttons1: 'https://www.google.com',
+      button2: 'https://www.facebook.com',
+    },
+    {
+      title: 'Project 2',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on rails',
+      badge2: 'CSS',
+      badge3: 'JavaScript',
+      badge4: 'HTML',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      buttons1: 'link',
+      button2: 'link2',
+    },
+    {
+      title: 'Project 3',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on rails',
+      badge2: 'CSS',
+      badge3: 'JavaScript',
+      badge4: 'HTML',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      buttons1: 'link',
+      button2: 'link2',
+    },
+    {
+      title: 'Project 4',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on rails',
+      badge2: 'CSS',
+      badge3: 'JavaScript',
+      badge4: 'HTML',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      buttons1: 'link',
+      button2: 'link2',
+    },
+    {
+      title: 'Project 5',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on rails',
+      badge2: 'CSS',
+      badge3: 'JavaScript',
+      badge4: 'HTML',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      buttons1: 'link',
+      button2: 'link2',
+    },
+    {
+      title: 'Project 6',
+      image: 'images/d0d9d4_solid_color_background_icolorpalette.png',
+      badge1: 'Ruby on rails',
+      badge2: 'CSS',
+      badge3: 'JavaScript',
+      badge4: 'HTML',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      button1: 'link',
+      button2: 'link2',
+    },
+  ];
+  function toggleBarWorks(workNumber) {
+    const y = document.getElementById('mobile-popup');
+    let i = 0;
+    if (workNumber === 1) {
+      i = 0;
+    } else if (workNumber === 2) {
+      i = 1;
+    } else if (workNumber === 3) {
+      i = 2;
+    } else if (workNumber === 4) {
+      i = 3;
+    } else if (workNumber === 5) {
+      i = 4;
+    } else if (workNumber === 6) {
+      i = 5;
+    }
+    document.getElementById('img-pop').src = projects[i].image;
+    document.getElementById('h3-popup').innerHTML = projects[i].title;
+    document.getElementById('badge1').innerHTML = projects[i].badge1;
+    document.getElementById('badge2').innerHTML = projects[i].badge2;
+    document.getElementById('badge3').innerHTML = projects[i].badge3;
+    document.getElementById('badge4').innerHTML = projects[i].badge4;
+    document.getElementById('p-pop').innerHTML = projects[i].description;
+    document.getElementById('btn-1').href = projects[i].button1;
+    document.getElementById('btn-2').href = projects[i].button2;
+  
+    if (y.style.display === 'flex') {
+      y.style.display = 'none';
+    } else {
+      y.style.display = 'flex';
+    }
+  }
+  
+  document.getElementById('xbut').addEventListener('click', toggleBarWorks);
+  document.getElementById('btnPopup1').addEventListener('click', () => { toggleBarWorks(1); });
+  document.getElementById('btnPopup2').addEventListener('click', () => { toggleBarWorks(2); });
+  document.getElementById('btnPopup3').addEventListener('click', () => { toggleBarWorks(3); });
+  document.getElementById('btnPopup4').addEventListener('click', () => { toggleBarWorks(4); });
+  document.getElementById('btnPopup5').addEventListener('click', () => { toggleBarWorks(5); });
+  document.getElementById('btnPopup6').addEventListener('click', () => { toggleBarWorks(6); });
+  
+});
+
