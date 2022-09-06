3246948052b2a34db35800a695929648e8f77e19
IngDavidTM <david-1011@hotmail.com>
2022-09-05 19:32:38 -0500

    fix linter errors on js 2

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-/* exported myFunction */
 function myFunction() { const x = document.getElementById('hide-menu'); const y = document.querySelector('.toolbar'); if (x.style.display === 'flex') { x.style.display = 'none'; y.style.display = 'flex'; } else { x.style.display = 'flex'; y.style.display = 'none'; } }
+document.getElementById('but').addEventListener('click', myFunction);
